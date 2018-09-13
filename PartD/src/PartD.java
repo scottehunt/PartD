157d2b0faa46973ab4490489e52d2f90c6c17765
scottehunt <scottehunt@gmail.com>
2018-09-13 13:20:35 +1200

    Second line of code written

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -3,6 +3,8 @@ public class PartD {
 
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Scott Hunt");
+		
+		System.out.println("My favourite colour is Blue");
 
 	}
 
