1475131adbfe3f51460cf9c7df25483080810601
scottehunt <scottehunt@gmail.com>
2018-09-13 13:21:34 +1200

    Third line of code written.

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -5,7 +5,8 @@ public class PartD {
 		System.out.println("Hi, my name is Scott Hunt");
 		
 		System.out.println("My favourite colour is Blue");
-
+		
+		System.out.println("My favourite mode of transportation is train");
 	}
 
 }
